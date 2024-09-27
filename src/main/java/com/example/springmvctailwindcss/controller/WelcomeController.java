16cc42e073b34e7db0d3db7ed879614d12b49340
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-09-27 20:38:39 +0630

    refactor codes and add files

--- src/main/java/com/example/springmvctailwindcss/controller/WelcomeController.java
+++ src/main/java/com/example/springmvctailwindcss/controller/WelcomeController.java
@@ -2,7 +2,7 @@ package com.example.springmvctailwindcss.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//npx tailwindcss -i ./src/main/resources/static/input.css -o ./src/main/resources/static/output.css --watch
 @Controller
 public class WelcomeController {
     @GetMapping("/")
